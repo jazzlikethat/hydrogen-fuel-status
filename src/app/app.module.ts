d07b178e384ff5b83841e3e5ce803309a4ac39de
Pradeep Kumar Aitha <paitha@buffalo.edu>
2019-09-21 09:36:45 -0700

    add ng-bootstrap; cleanup homepage;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; // TODO: Alternatively you could only import modules with components you need, ex. pagination and alert. The resulting bundle will be smaller in this case.
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
